03ceb7a75581815a52c0891662fed1363f2fa410
Lzc <752143280@qq.com>
2017-06-16 14:06:59 +0800

    完成无刷新pjax功能

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,5 +1,5 @@
 /**
- * Created by Lzc on 2017/5/20.
+ * Created by Lzc.
  */
 $sidebar = $("#sidebar");
 $content = $("#content");
@@ -8,62 +8,60 @@ $nav = $("#nav");
 $navMenuBtn = $("#nav .menu");
 $navMenu = $("#nav ul li");
 $main = $("#main");
-var list = ["home","skill","document","communion","about"];
+var list = ["home", "skill", "document", "communion", "about"];
 
 //展开侧边栏
-function openSidebar(){
-    $nav.animate({width:"200px"},300);
-    $content.animate({marginLeft:"200px"},300);
+function openSidebar() {
+    $nav.animate({ width: "200px" }, 300);
+    $content.animate({ marginLeft: "200px" }, 300);
     var add = 0;
-    var alphaAdd_jsq = setInterval(function ()
-    {
-        add+=0.1;
-        $mask.css({backgroundColor: "rgba(0,0,0,"+ add +")"});
-        add >= 0.3?clearInterval(alphaAdd_jsq):add;
-    },50);
+    var alphaAdd_jsq = setInterval(function () {
+        add += 0.1;
+        $mask.css({ backgroundColor: "rgba(0,0,0," + add + ")" });
+        add >= 0.3 ? clearInterval(alphaAdd_jsq) : add;
+    }, 50);
     $navMenuBtn.animate({
-        width:30,
-        height:30
-    },300);
-    if($nav.width() < 200)
-        jua.rotate($navMenuBtn,0,-45,300);
+        width: 30,
+        height: 30
+    }, 300);
+    if ($nav.width() < 200)
+        jua.rotate($navMenuBtn, 0, -45, 300);
 }
 
 //收起侧边栏
-function closeSidebar(event){
+function closeSidebar(event) {
     event.stopPropagation();
-    $nav.animate({width:"40px"},300);
-    $content.animate({marginLeft:"40px"},300);
+    $nav.animate({ width: "40px" }, 300);
+    $content.animate({ marginLeft: "40px" }, 300);
     var reduce = 0.3;
-    var alphaReduce_jsq = setInterval(function ()
-    {
+    var alphaReduce_jsq = setInterval(function () {
         reduce -= 0.1;
-        $mask.css({backgroundColor: "rgba(0,0,0,"+ reduce +")"});
-        reduce < 0?clearInterval(alphaReduce_jsq):reduce;
-    },50);
+        $mask.css({ backgroundColor: "rgba(0,0,0," + reduce + ")" });
+        reduce < 0 ? clearInterval(alphaReduce_jsq) : reduce;
+    }, 50);
     $navMenuBtn.animate({
-        width:22,
-        height:22
-    },300);
-    if($nav.width() > 40)
-        jua.rotate($navMenuBtn,-45,0,300);
+        width: 22,
+        height: 22
+    }, 300);
+    if ($nav.width() > 40)
+        jua.rotate($navMenuBtn, -45, 0, 300);
 }
 
 //侧边栏点击事件
-$sidebar.on("click",openSidebar);
-$content.add($navMenuBtn).on("click",function (event)
-{
-    if($nav.width() > 40)
+$sidebar.on("click", openSidebar);
+$content.add($navMenuBtn).on("click", function (event) {
+    if ($nav.width() > 40)
         closeSidebar(event);
 });
 
 //导航菜单动画
-$navMenu.on("click",function(event){
+$navMenu.on("click", function (event) {
     var $that = $(this);
-    $navMenu.children().removeClass("active");
-    $that.siblings().animate({paddingLeft:0},200);
-    $that.animate({paddingLeft:10},200);
-    $that.children("a").addClass("active");
+    changeNavMenu($that);
+    // $navMenu.children().removeClass("active");
+    // $that.siblings().animate({ paddingLeft: 0 }, 200);
+    // $that.animate({ paddingLeft: 10 }, 200);
+    // $that.children("a").addClass("active");
     /*setTimeout(function ()//超时自动收起侧边栏
      {
      closeSidebar(event);
@@ -82,82 +80,99 @@ $(document).ready(function() {
          }
      });
  });*/
-//首先加载一次首页
-$.ajax({
-    type:"get",
-    url:"home.html",
-    dateType:"text",
-    success:function (txt)
+ //导航菜单动画
+//  function changeNavMenu()
+//     {
+//         var title = $main.children().attr("class");
+//         for(var i in list)
+//         {
+//             if(list[i] == title)
+//             {
+//                 $navMenu.children().removeClass("active");
+//                 $navMenu.eq(i).siblings().animate({paddingLeft:0},200);
+//                 $navMenu.eq(i).animate({paddingLeft:10},200);
+//                 $navMenu.eq(i).children("a").addClass("active");
+//             }
+//         }
+//     }
+function changeNavMenu(navMenu)
     {
-        $main.html(txt).hide().fadeIn(1000);
-    },
-    error:function ()
-    {
-        console.log("错误");
+        $navMenu.children().removeClass("active");
+        navMenu.siblings().animate({paddingLeft:0},200);
+        navMenu.animate({paddingLeft:10},200);
+        navMenu.children("a").addClass("active");
     }
-})
-//给a标签绑定pjax事件
-$sidebar.pjax('a','#main',{timeout:3000});
-//入场动画
-
-$(document).on("pjax:success",function(options, data, status, xhr){
-    //data:返回的内容
-    //status:状态(如:success)
-    //xhr:XMLHttpRequest对象
-    //contents:返回的jquery对象
-    $main.hide().fadeIn(1000);
-    /*var chList = ["首页","雕虫小技","IE那些事","吐槽本站","个人资料"];
-    var url = window.location.pathname;
-    for(var i = 0; i < list.length; i++)
-    {
-        if(url.indexOf(list[i]) != -1)
-            $("title").text(chList[i]);
-    }*/
-})
-
+    
 //导航栏跟随前进后退事件变化
 window.onpopstate = function()
 {
-    setTimeout(function ()
+    var title = $main.children().attr("class");
+    for(var i in list)
     {
-        var title = $main.children().attr("class");
-        for(var i in list)
-        {
-            if(list[i] == title)
-            {
-                $navMenu.children().removeClass("active");
-                $navMenu.eq(i).siblings().animate({paddingLeft:0},200);
-                $navMenu.eq(i).animate({paddingLeft:10},200);
-                $navMenu.eq(i).children("a").addClass("active");
-            }
+        if(list[i] == title){
+            setTimeout(changeNavMenu($navMenu.eq(i)),100);
         }
-    },100);
+    }
 }
-/*
-//ajax插件(支持histroy功能)
+
+//首先加载一次首页
+var onhash = window.location.hash.replace(/#\//,"");
+if (onhash) {
+    $.ajax({
+        type: "get",
+        url: onhash,
+        dateType: "html",
+        success: function (txt) {
+            $main.html(txt).hide().fadeIn(1000);
+            $(window).trigger("popstate");
+        },
+        error: function () {
+            console.log("url链接错误");
+        }
+    });
+}
+
+//给a标签绑定pjax事件
 $.pjax({
-    selector: 'a',
+    selector: 'a[data-pjax]',
     container: '#main', //内容替换的容器
     show: 'fade',  //展现的动画，支持默认和fade, 可以自定义动画方式，这里为自定义的function即可。
     cache: false,  //是否使用缓存
     storage: false,  //是否使用本地存储
+    timeout: 100000,
     titleSuffix: "", //标题后缀
-    filter: function(){},
-    callback: function(status){
+    filter: function () { },
+    callback: function (status) {
         var type = status.type;
-        switch(type){
-            case 'success':
-                //$("title").text($(this).data("name"));//修改title
-                $('<link rel="stylesheet" href="css/'+ $(this).data("url") +'.css">').appendTo($("head"));//动态添加CSS
+        switch (type) {
+            case 'success':;
+                //$('<link rel="stylesheet" href="css/'+ $(this).data("url") +'.css">').appendTo($("head"));//动态添加CSS
                 break;//正常
-            case 'cache':;break; //读取缓存
-            case 'error': ;break; //发生异常
-            case 'hash': ;break; //只是hash变化
+            case 'cache': ; break; //读取缓存
+            case 'error': ; break; //发生异常
+            case 'hash': ; break; //只是hash变化
         }
     }
-})
-*/
+});
 
+
+// $sidebar.pjax('a','#main',{timeout:3000});
+// //入场动画
+
+// $(document).on("pjax:success",function(options, data, status, xhr){
+//     //data:返回的内容
+//     //status:状态(如:success)
+//     //xhr:XMLHttpRequest对象
+//     //contents:返回的jquery对象
+//     $main.hide().fadeIn(1000);
+//     /*var chList = ["首页","雕虫小技","IE那些事","吐槽本站","个人资料"];
+//     var url = window.location.pathname;
+//     for(var i = 0; i < list.length; i++)
+//     {
+//         if(url.indexOf(list[i]) != -1)
+//             $("title").text(chList[i]);
+//     }*/
+// })
 
  /*function getQueryString(name)
  {
