47208dcef55ebc1a38bfcd7dad7dc4ce46e558c1
Lzc <752143280@qq.com>
2017-06-22 22:03:45 +0800

    新增concent加载动画,侧边栏新增当前天气信息

--- js/sidebar.js
+++ js/sidebar.js
@@ -3,7 +3,7 @@
  */
 $sidebar = $("#sidebar");
 $content = $("#content");
-$mask = $("#content > div");
+$mask = $("#content .mask");
 $nav = $("#nav");
 $navMenuBtn = $("#nav .menu");
 $navMenu = $("#nav ul li");
@@ -12,12 +12,18 @@ var list = ["home", "skill", "document", "communion", "about"];
 
 //展开侧边栏
 function openSidebar() {
-    $nav.animate({ width: "200px" }, 300);
-    $content.animate({ marginLeft: "200px" }, 300);
+    $nav.animate({
+        width: "200px"
+    }, 300);
+    $content.animate({
+        marginLeft: "200px"
+    }, 300);
     var add = 0;
     var alphaAdd_jsq = setInterval(function () {
         add += 0.1;
-        $mask.css({ backgroundColor: "rgba(0,0,0," + add + ")" });
+        $mask.css({
+            backgroundColor: "rgba(0,0,0," + add + ")"
+        });
         add >= 0.3 ? clearInterval(alphaAdd_jsq) : add;
     }, 50);
     $navMenuBtn.animate({
@@ -31,12 +37,18 @@ function openSidebar() {
 //收起侧边栏
 function closeSidebar(event) {
     event.stopPropagation();
-    $nav.animate({ width: "40px" }, 300);
-    $content.animate({ marginLeft: "40px" }, 300);
+    $nav.animate({
+        width: "40px"
+    }, 300);
+    $content.animate({
+        marginLeft: "40px"
+    }, 300);
     var reduce = 0.3;
     var alphaReduce_jsq = setInterval(function () {
         reduce -= 0.1;
-        $mask.css({ backgroundColor: "rgba(0,0,0," + reduce + ")" });
+        $mask.css({
+            backgroundColor: "rgba(0,0,0," + reduce + ")"
+        });
         reduce < 0 ? clearInterval(alphaReduce_jsq) : reduce;
     }, 50);
     $navMenuBtn.animate({
@@ -54,69 +66,40 @@ $content.add($navMenuBtn).on("click", function (event) {
         closeSidebar(event);
 });
 
-//导航菜单动画
+//导航菜单动画实现
+function changeNavMenu(navMenu) {
+    $navMenu.children().removeClass("active");
+    navMenu.siblings().animate({
+        paddingLeft: 0
+    }, 200);
+    navMenu.animate({
+        paddingLeft: 10
+    }, 200);
+    navMenu.children("a").addClass("active");
+}
+
+//导航菜单点击事件
 $navMenu.on("click", function (event) {
     var $that = $(this);
     changeNavMenu($that);
-    // $navMenu.children().removeClass("active");
-    // $that.siblings().animate({ paddingLeft: 0 }, 200);
-    // $that.animate({ paddingLeft: 10 }, 200);
-    // $that.children("a").addClass("active");
-    /*setTimeout(function ()//超时自动收起侧边栏
-     {
-     closeSidebar(event);
-     },1000);*/
+    //捕获点击事件
+    $(document).on('click',function(){
+        return false;
+    });
 });
 
-/*//禁止F5刷新
-$(document).ready(function() {
-     $(document).bind("keydown", function(e)
-     {
-         e = window.event || e;
-         if (e.keyCode == 116)
-         {
-             e.keyCode = 0;
-             return false;
-         }
-     });
- });*/
- //导航菜单动画
-//  function changeNavMenu()
-//     {
-//         var title = $main.children().attr("class");
-//         for(var i in list)
-//         {
-//             if(list[i] == title)
-//             {
-//                 $navMenu.children().removeClass("active");
-//                 $navMenu.eq(i).siblings().animate({paddingLeft:0},200);
-//                 $navMenu.eq(i).animate({paddingLeft:10},200);
-//                 $navMenu.eq(i).children("a").addClass("active");
-//             }
-//         }
-//     }
-function changeNavMenu(navMenu)
-    {
-        $navMenu.children().removeClass("active");
-        navMenu.siblings().animate({paddingLeft:0},200);
-        navMenu.animate({paddingLeft:10},200);
-        navMenu.children("a").addClass("active");
-    }
-    
-//导航栏跟随前进后退事件变化
-window.onpopstate = function()
-{
+//导航菜单跟随前进后退事件变化
+window.onpopstate = function () {
     var title = $main.children().attr("class");
-    for(var i in list)
-    {
-        if(list[i] == title){
-            setTimeout(changeNavMenu($navMenu.eq(i)),100);
+    for (var i in list) {
+        if (list[i] == title) {
+            setTimeout(changeNavMenu($navMenu.eq(i)), 100);
         }
     }
 }
 
 //首先加载一次首页
-var onhash = window.location.hash.replace(/#\//,"");
+var onhash = window.location.hash.replace(/#\//, "");
 if (onhash) {
     $.ajax({
         type: "get",
@@ -136,26 +119,107 @@ if (onhash) {
 $.pjax({
     selector: 'a[data-pjax]',
     container: '#main', //内容替换的容器
-    show: 'fade',  //展现的动画，支持默认和fade, 可以自定义动画方式，这里为自定义的function即可。
-    cache: false,  //是否使用缓存
-    storage: false,  //是否使用本地存储
+    show: 'fade', //展现的动画，支持默认和fade, 可以自定义动画方式，这里为自定义的function即可。
+    cache: false, //是否使用缓存
+    storage: false, //是否使用本地存储
     timeout: 100000,
     titleSuffix: "", //标题后缀
-    filter: function () { },
+    filter: function () {},
     callback: function (status) {
         var type = status.type;
         switch (type) {
-            case 'success':;
+            case 'success':
+                ;
                 //$('<link rel="stylesheet" href="css/'+ $(this).data("url") +'.css">').appendTo($("head"));//动态添加CSS
-                break;//正常
-            case 'cache': ; break; //读取缓存
-            case 'error': ; break; //发生异常
-            case 'hash': ; break; //只是hash变化
+                break; //正常
+            case 'cache':
+                ;
+                break; //读取缓存
+            case 'error':
+                ;
+                break; //发生异常
+            case 'hash':
+                ;
+                break; //只是hash变化
         }
     }
 });
 
+$main.on('pjax.start', function () {
+    $(".content_loading_animation").show();
+}).on('pjax.end', function () {
+    $(".content_loading_animation").hide();
+    closeSidebar(event);
+    ////解除捕获点击事件
+    $(document).off('click',function(){
+    });
+});
 
+(function () {
+    var i = 0;
+    //a: X轴的一半, b: Y轴的一半, r: 圆的半径);
+    function dotPosition() {
+        var a = $(".content_loading_animation").width() / 2;
+        var b = $(".content_loading_animation").height() / 2;
+        var r = a;
+        var $dot = $(".content_loading_animation ul li");
+        var angle = [0, 30, 60, 90, 120, 150, 180, 210, 240, 270, 300, 330];
+        for (var i = 0; i < angle.length; i++) {
+            var radian = (2 * Math.PI / 360) * angle[i] //固定公式
+            $dot[i].style.left = parseInt(a + Math.sin(radian) * r) - 9 + "px";
+            $dot[i].style.top = parseInt(b + Math.cos(radian) * r) - 9 + "px";
+        }
+    }
+    dotPosition();
+
+    var canvas = document.getElementById("canvas");
+    var ctx = canvas.getContext('2d');
+    ctx.lineWidth = 4;
+    drawNew(ctx, 0, 0);
+
+    function drawNew(ctx, begin, end) {
+        ctx.clearRect(0, 0, 100, 100);
+        ctx.beginPath();
+        ctx.strokeStyle = "#555";
+        if (end < Math.PI * 2) {
+            end += Math.PI / 50;
+        } else {
+            begin += Math.PI / 50;
+            end = Math.PI * 2;
+            if (begin >= Math.PI * 2 && end >= Math.PI * 2) {
+                begin = 0;
+                end = 0;
+            }
+        };
+        ctx.arc(30, 20, 15, begin, end, false);
+        ctx.stroke();
+        // 360° = Math.PI*2;
+        requestAnimationFrame(function () {
+            drawNew(ctx, begin, end);
+        });
+    }
+})();
+
+//天气数据加载
+(function () {
+    $.getScript("http://int.dpool.sina.com.cn/iplookup/iplookup.php?format=js", function (_result) {
+            getWeather(remote_ip_info.city);
+        });
+        function getWeather(city) {
+            $.ajax({
+                type: "get",
+                dataType: "json",
+                url: "https://free-api.heweather.com/v5/weather?city="+ city +"&key=763188a5a5fc4e5fb55913009e0ba0c5",
+                success: function (data) {
+                    $(".weather .tmp span").text(data.HeWeather5[0].now.tmp);//当前温度
+                    $(".weather .cond span").text(data.HeWeather5[0].now.cond.txt);//当前天气
+                    $(".weather .city span").text(data.HeWeather5[0].basic.city);//当前城市
+                    $(".weather .wind span").text(data.HeWeather5[0].now.wind.sc);//当前风速
+                    $(".weather .aqi span").text(data.HeWeather5[0].aqi.city.qlty);//空气质量
+                }
+            });
+        };
+})();
 // $sidebar.pjax('a','#main',{timeout:3000});
 // //入场动画
 
@@ -174,12 +238,6 @@ $.pjax({
 //     }*/
 // })
 
- /*function getQueryString(name)
- {
- var reg = new RegExp("(^|&)"+ name +"=([^&]*)(&|$)");
- var r = window.location.search.substr(1).match(reg);
- if(r!=null)return  unescape(r[2]); return null;
- }*/
 /*function getPage(target)
 {
     /*var list = {
